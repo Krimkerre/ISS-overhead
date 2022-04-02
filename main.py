46d64d55ce4a9baf564348f4343d64230e967755
Krimkerre <66228690+Krimkerre@users.noreply.github.com>
2022-04-02 13:38:54 +0200

    add day/night checking and mail notification

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 from datetime import datetime
+import smtplib
+import time
 
-MY_LAT = 51.275910 # Your latitude
-MY_LONG = -3.627300 # Your longitude
-
+MY_LAT = 51.275910  # Your latitude
+MY_LONG = -3.627300  # Your longitude
+MY_EMAIL = "supersecret@gmail.com"  # enter your email here
+MY_PASSWORD = "Sup3rS3cure"  # enter your password here
 
 def is_iss_overhead():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -13,8 +16,8 @@ def is_iss_overhead():
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
 
-    #Your position is within +5 or -5 degrees of the ISS position.
-    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+    # Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
         return True
 
 
@@ -36,11 +39,14 @@ def is_night():
     if time_now >= sunset or time_now <= sunrise:
         return True
 
-
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
-
-
-
+while True:
+    time.sleep(60)                                                      # Keeps code running and refreshing every minute
+    if is_iss_overhead() and is_night():
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:    # Change SMTP and port number here if not gmail
+            connection.starttls()
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=MY_EMAIL,
+                msg=f"Subject: Look up!\n\nThe ISS is above you!"
+            )
